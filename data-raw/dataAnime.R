3c421cc4e23ebbea8f0c6bad6ae14f5743167a4b
UJANG FAHMI <eppofahmi@gmail.com>
2019-07-15 12:26:54 +0700

    delete raw data

--- data-raw/dataAnime.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-dataAnime <- read_csv("data-raw/dataAnime.csv", 
-                       trim_ws = FALSE)
-
-# glimpse(dataanime)
-
-write_csv(dataAnime, "data-raw/dataAnime.csv")
-save(dataAnime, file = "data/dataAnime.rda", compress = "xz")
