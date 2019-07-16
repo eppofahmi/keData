e9eb6385b745cb36939e2c892577d7a6aa481c4a
UJANG FAHMI <eppofahmi@gmail.com>
2019-07-16 11:23:03 +0700

    adding ml and dl kaggle survey

---
+++ R/mldlSurvey.R
@@ -0,0 +1,15 @@
+#' Kaggle ML & DS Survey
+#'
+#' Kaggle ML & DS Survey 2018
+#'
+#' @source https://www.kaggle.com/kaggle/kaggle-survey-2018/downloads/kaggle-survey-2018.zip/5
+#'
+#' @format Large list with 3 elements
+#' \describe{
+#' \item{multipleChoiceResponses}{data frame with 35 varibales and 23860 observations}
+#' \item{freeFormResponses}{data frame with 395 varibales and 23860 observations}
+#' \item{SurveySchema}{data frame with 52 variables and 52 observations}
+#' }
+#' @examples
+#'   mldlSurvey
+"mldlSurvey"
