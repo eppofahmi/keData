4b158ad9483eeea2de66acf58d55bfebe681d502
UJANG FAHMI <eppofahmi@gmail.com>
2019-07-22 10:57:25 +0700

    cleaning data twit

--- data-raw/kedataTwit.R
+++ data-raw/kedataTwit.R
@@ -5,6 +5,13 @@ kedataTwit <- read_delim("data-raw/kedataTwit.csv",
 
 colnames(kedataTwit) <- c("created_at", "time", "username", "text", "is_retweet", 
                         "reply", "retweet", "favorite", "source")
+
+kedataTwit <- kedataTwit %>% 
+  filter(source != "NA") %>% 
+  filter(!str_detect(time, "yayasan pemerhati kanker"))
+
+kedataTwit <- kedataTwit[-c(1:3), ]
+
 # glimpse(kemenkes)
 write_csv(kedataTwit, "data-raw/kedataTwit.csv")
 save(kedataTwit, file = "data/kedataTwit.rda", compress = "xz")
